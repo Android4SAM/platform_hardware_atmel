ff1851340378ec113b6da0e585d890fc41d0fae2
LiuXin <liuxing@embedinfo.com>
2014-04-24 02:56:41 -0400

    gralloc: fix the lcd flicker problem
    fix the problem that lcd will flicker under android.
    because the fb_post function use FBIOPUT_VSCREENINFO.
    
    Signed-off-by: LiuXin <liuxing@embedinfo.com>

--- sama5dx/gralloc/framebuffer.cpp
+++ sama5dx/gralloc/framebuffer.cpp
@@ -97,7 +97,7 @@ static int fb_post(struct framebuffer_device_t* dev, buffer_handle_t buffer)
         const size_t offset = hnd->base - m->framebuffer->base;
         m->info.activate = FB_ACTIVATE_VBL;
         m->info.yoffset = offset / m->finfo.line_length;
-        if (ioctl(m->framebuffer->fd, FBIOPUT_VSCREENINFO, &m->info) == -1) {
+        if (ioctl(m->framebuffer->fd, FBIOPAN_DISPLAY, &m->info) == -1) {
             ALOGE("FBIOPUT_VSCREENINFO failed");
             m->base.unlock(&m->base, buffer); 
             return -errno;
