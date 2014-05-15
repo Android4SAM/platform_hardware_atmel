58ee8baa92752d22c0e7cbb77492fbde48db2971
Royce Jiang <jiangxd@embedinfo.com>
2014-05-15 10:42:37 +0800

    build: add build support for sama5d4
    Signed-off-by: Royce Jiang <jiangxd@embedinfo.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,6 @@
 ifneq ($(filter sama5d3-ek, $(TARGET_BOOTLOADER_BOARD_NAME)),)
 include $(call all-named-subdir-makefiles,sama5dx)
 endif
+ifneq ($(filter sama5d4-ek, $(TARGET_BOOTLOADER_BOARD_NAME)),)
+include $(call all-named-subdir-makefiles,sama5dx)
+endif
