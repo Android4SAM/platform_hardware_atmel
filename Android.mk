b24c56f95a0f14e9421ad7daf4dd13b0f9eb3001
Royce Jiang <jiangxd@embest-tech.com>
2015-08-19 14:10:35 +0800

    build: add support for sama5d2
    Signed-off-by: Royce Jiang <jiangxd@embest-tech.com>

--- Android.mk
+++ Android.mk
@@ -4,3 +4,6 @@ endif
 ifneq ($(filter sama5d4-ek, $(TARGET_BOOTLOADER_BOARD_NAME)),)
 include $(call all-named-subdir-makefiles,sama5dx)
 endif
+ifneq ($(filter sama5d2-ek, $(TARGET_BOOTLOADER_BOARD_NAME)),)
+include $(call all-named-subdir-makefiles,sama5dx)
+endif
